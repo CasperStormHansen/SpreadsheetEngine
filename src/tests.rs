1d241fce0a10a258064155adce06f02b7e056b94
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2026-04-02 11:50:51 +0200

    Add tests for circular references and formula chains in spreadsheet cells

--- src/tests.rs
+++ src/tests.rs
@@ -38,7 +38,7 @@ fn single_cell_number_literal() {
 }
 
 #[test]
-fn single_cell_self_reference() {
+fn single_self_referencing_cell() {
     let mut spreadsheet = Spreadsheet::new();
     spreadsheet.input_raw_formula(adr![0, 0], "(0,0)");
 
@@ -54,7 +54,7 @@ fn single_cell_self_reference() {
 }
 
 #[test]
-fn single_cell_invalid_formula() {
+fn single_cell_with_invalid_formula() {
     let mut spreadsheet = Spreadsheet::new();
     spreadsheet.input_raw_formula(adr![0, 0], "invalid formula");
 
@@ -102,5 +102,100 @@ fn one_cell_referencing_another() {
         children: set![],
         value: Number(1.0),
         parents: set![adr![0, 0]],
+    });
+}
+
+#[test]
+fn two_cells_referencing_each_other() {
+    let mut spreadsheet = Spreadsheet::new();
+    spreadsheet.input_raw_formula(adr![0, 0], "(0,1)");
+
+    assert_eq!(spreadsheet.cells.len(), 1);
+    assert_eq!(spreadsheet.cells[& adr![0, 0]], Cell {
+        raw_formula: "(0,1)".to_string(),
+        parsed_formula: CellReference(adr![0, 1]),
+        child_regions: set![SingleCell(adr![0, 1])],
+        children: set![],
+        value: Number(0.0),
+        parents: set![],
+    });
+
+    spreadsheet.input_raw_formula(adr![0, 1], "(0,0)");
+
+    assert_eq!(spreadsheet.cells.len(), 2);
+    assert_eq!(spreadsheet.cells[& adr![0, 0]], Cell {
+        raw_formula: "(0,1)".to_string(),
+        parsed_formula: CellReference(adr![0, 1]),
+        child_regions: set![SingleCell(adr![0, 1])],
+        children: set![adr![0, 1]],
+        value: Unevaluated,
+        parents: set![adr![0, 1]],
+    });
+    assert_eq!(spreadsheet.cells[& adr![0, 1]], Cell {
+        raw_formula: "(0,0)".to_string(),
+        parsed_formula: CellReference(adr![0, 0]),
+        child_regions: set![SingleCell(adr![0, 0])],
+        children: set![adr![0, 0]],
+        value: Unevaluated,
+        parents: set![adr![0, 0]],
+    });
+}
+
+#[test]
+fn one_cell_referencing_self_referencing_cell() {
+    let mut spreadsheet = Spreadsheet::new();
+    spreadsheet.input_raw_formula(adr![0, 0], "(0,0)");
+    spreadsheet.input_raw_formula(adr![0, 1], "(0,0)");
+
+    assert_eq!(spreadsheet.cells.len(), 2);
+    assert_eq!(spreadsheet.cells[& adr![0, 0]], Cell {
+        raw_formula: "(0,0)".to_string(),
+        parsed_formula: CellReference(adr![0, 0]),
+        child_regions: set![SingleCell(adr![0, 0])],
+        children: set![adr![0, 0]],
+        value: Unevaluated,
+        parents: set![adr![0, 0], adr![0, 1]],
+    });
+    assert_eq!(spreadsheet.cells[& adr![0, 1]], Cell {
+        raw_formula: "(0,0)".to_string(),
+        parsed_formula: CellReference(adr![0, 0]),
+        child_regions: set![SingleCell(adr![0, 0])],
+        children: set![adr![0, 0]],
+        value: Unevaluated,
+        parents: set![],
+    });
+}
+
+#[test]
+fn chain_of_three_cells() {
+    let mut spreadsheet = Spreadsheet::new();
+    spreadsheet.input_raw_formula(adr![0, 0], "(0,1)");
+    spreadsheet.input_raw_formula(adr![0, 1], "(0,2)");
+    spreadsheet.input_raw_formula(adr![0, 2], "1.1");
+
+    assert_eq!(spreadsheet.cells.len(), 3);
+    assert_eq!(spreadsheet.cells[& adr![0, 0]], Cell {
+        raw_formula: "(0,1)".to_string(),
+        parsed_formula: CellReference(adr![0, 1]),
+        child_regions: set![SingleCell(adr![0, 1])],
+        children: set![adr![0, 1]],
+        value: Number(1.1),
+        parents: set![],
+    });
+    assert_eq!(spreadsheet.cells[& adr![0, 1]], Cell {
+        raw_formula: "(0,2)".to_string(),
+        parsed_formula: CellReference(adr![0, 2]),
+        child_regions: set![SingleCell(adr![0, 2])],
+        children: set![adr![0, 2]],
+        value: Number(1.1),
+        parents: set![adr![0, 0]],
+    });
+    assert_eq!(spreadsheet.cells[& adr![0, 2]], Cell {
+        raw_formula: "1.1".to_string(),
+        parsed_formula: NumberLiteral(1.1),
+        child_regions: set![],
+        children: set![],
+        value: Number(1.1),
+        parents: set![adr![0, 1]],
     });
 }
