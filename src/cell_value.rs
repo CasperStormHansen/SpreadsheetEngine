4360e731c3c16e31618a2a9488f655003be1fdb7
[Casper Storm Hansen] <casper_storm_hansen@hotmail.com>
2026-04-02 12:46:10 +0200

    Add TODO comment for addressing floating-point precision issue in `CellValue::Number`

--- src/cell_value.rs
+++ src/cell_value.rs
@@ -1,6 +1,6 @@
 #[derive(PartialEq, Debug, Clone)]
 pub(crate) enum CellValue {
-    Number(f64),
+    Number(f64), // todo: To be improved. This choice means that 0.1 + 0.2 != 0.3 due to floating point precision issues.
     Error(String),
     Unevaluated,
 }
